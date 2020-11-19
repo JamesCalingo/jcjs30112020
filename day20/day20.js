51dbff10d0410aaf4c88166a7ff1f094d2111e68
JC <james.calingo@gmail.com>
2020-11-19 12:16:12 -0500

    I need to test something with day 20

---
+++ day20/day20.js
@@ -0,0 +1,30 @@
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+
+const recognition = new SpeechRecognition()
+recognition.interimResults = true
+
+let p = document.createElement("p")
+const text = document.querySelector(".text")
+text.appendChild(p)
+
+recognition.addEventListener("result", event => {
+  const transcript = Array.from(event.results)
+  .map(result => result[0])
+  .map(result => result.transcript)
+  .join("")
+
+  const censored = transcript.replace(/sus/gi, "I AM NOT SUS I SWEAR")
+  p.textContent = censored
+
+
+  if(event.results[0].isFinal){
+    p = document.createElement('p')
+    text.appendChild(p)
+  }
+  
+})
+
+
+recognition.addEventListener("end", recognition.start)
+
+recognition.start()
