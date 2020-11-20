14d710bc514efe5729922d21453906042ecf7a07
JC <james.calingo@gmail.com>
2020-11-20 12:51:27 -0500

    reimplemented Math.round

--- day21/day21.js
+++ day21/day21.js
@@ -1,13 +1,15 @@
 const arrow = document.querySelector('.arrow')
 const speed = document.querySelector('.speed-value')
 
+alert("Just a fair warning: This compass and speedometer may not work on your device. To see why, use the read more link.")
+
 navigator.geolocation.watchPosition(function(data) {
   console.log(`DATA AT ${data.timestamp}
   speed: ${data.coords.speed},
   direction: ${data.coords.heading},
   lat: ${data.coords.latitude},
   lon: ${data.coords.longitude}`,)
-  speed.textContent = data.coords.speed
+  speed.textContent = Math.round(data.coords.speed)
   arrow.getElementsByClassName.transform = `rotate(${data.coords.heading}deg)`
 }, (err) => {
   console.error(err)
