aec857cd46e5f0ab2f38ac9c49b3ec27910d3628
JC <james.calingo@gmail.com>
2020-11-20 11:14:57 -0500

    another console update

--- day21/day21.js
+++ day21/day21.js
@@ -2,10 +2,11 @@ const arrow = document.querySelector('.arrow')
 const speed = document.querySelector('.speed-value')
 
 navigator.geolocation.watchPosition(function(data) {
-  console.log(`SPEED: ${data.coords.speed},
-  DIRECTION: ${data.coords.heading},
-  LATITUDE: ${data.coords.latitude},
-  LONGITUDE: ${data.coords.longitude}`,)
+  console.log(`DATA AT ${data.timestamp}
+  speed: ${data.coords.speed},
+  direction: ${data.coords.heading},
+  lat: ${data.coords.latitude},
+  lon: ${data.coords.longitude}`,)
   speed.textContent = data.coords.speed
   arrow.getElementsByClassName.transform = `rotate(${data.coords.heading}deg)`
 }, (err) => {
