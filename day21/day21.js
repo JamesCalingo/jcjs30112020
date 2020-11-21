4215eca7d086b1b75d41e1c910fb795fefd9a057
JC <james.calingo@gmail.com>
2020-11-21 09:53:43 -0500

    final(?) day 21 update

--- day21/day21.js
+++ day21/day21.js
@@ -1,18 +1,21 @@
-const arrow = document.querySelector('.arrow')
-const speed = document.querySelector('.speed-value')
-const latlon = document.querySelector('.latlon')
+const arrow = document.querySelector(".arrow");
+const speed = document.querySelector(".speed-value");
+const latlon = document.querySelector(".latlon");
 
-navigator.geolocation.watchPosition(function(data) {
-  console.log(`DATA AT ${data.timestamp}
+navigator.geolocation.watchPosition(
+  function (data) {
+    console.log(`DATA AT ${data.timestamp}
   speed: ${data.coords.speed},
   direction: ${data.coords.heading},
   lat: ${data.coords.latitude},
-  lon: ${data.coords.longitude}`,)
-  speed.textContent = Math.round(data.coords.speed)
-  latlon.innerHTML = `Latitude: ${data.coords.latitude}<br>
-  Longitude: ${data.coords.longitude}`
-  arrow.style.transform = `rotate(${data.coords.heading}deg)`
-}, (err) => {
-  console.error(err)
-  alert("This app needs location permissions to work.")
-})
+  lon: ${data.coords.longitude}`);
+    speed.textContent = Math.round(data.coords.speed);
+    latlon.innerHTML = `Latitude: ${data.coords.latitude}<br>
+  Longitude: ${data.coords.longitude}`;
+    arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+  },
+  (err) => {
+    console.error(err);
+    alert("This app needs location permissions to work.");
+  }
+);
