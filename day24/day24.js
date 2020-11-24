e3d5b65bad636dba7b8c914eeddbbde9f0ed1972
JC <james.calingo@gmail.com>
2020-11-24 09:36:19 -0500

    day 24

---
+++ day24/day24.js
@@ -0,0 +1,14 @@
+const nav = document.querySelector("#main")
+const navTop = nav.offsetTop
+
+function fixNav() {
+if(window.scrollY >= navTop) {
+  document.body.style.paddingTop = nav.offsetHeight + 'px'
+  document.body.classList.add("nav-fixed")
+}else{
+  document.body.style.paddingTop = 0
+  document.body.classList.remove("nav-fixed")
+}
+}
+
+window.addEventListener("scroll", fixNav)
