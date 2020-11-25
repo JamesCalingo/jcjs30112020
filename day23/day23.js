8e6d96122f4cb81a056581be71c339fcc14ba611
JC <james.calingo@gmail.com>
2020-11-25 18:39:46 -0500

    "day 26"

--- day23/day23.js
+++ day23/day23.js
@@ -6,9 +6,6 @@ const speakBtn = document.querySelector("#speak")
 const stopBtn = document.querySelector("#stop")
 msg.text = "You left the text box empty...so let me tell you something: I want to be the very best Like no one ever was To catch them is my real test To train them is my cause I will travel across the land Searching far and wide Each Pokemon to understand The power that's inside Pokemon Gotta catch em all It's you and me I know it's my destiny Pokemon Oh, you're my best friend In a world we must defend Pokemon Gotta catch em' all A heart so true Our courage will pull us through You teach me and I'll teach you Pokemon Gotta catch em' all Gotta catch em' all Pokemon"
 
-
-
-
 function populateVoices() {
   voices = this.getVoices()
   const dropdown = voices
---
+++ day26/day26.js
@@ -0,0 +1,34 @@
+const triggers = document.querySelectorAll(".navBar > li")
+const background = document.querySelector(".dropdownBG")
+const nav = document.querySelector(".top")
+
+function handleEnter () {
+this.classList.add('trigger-enter')
+setTimeout(() => {
+  if(this.classList.contains("trigger-enter")){
+    this.classList.add('trigger-enter-active')
+  }
+}, 150)
+background.classList.add('open')
+const dropdown = this.querySelector(".dropdown")
+const ddCoords = dropdown.getBoundingClientRect()
+const navCoords = nav.getBoundingClientRect()
+const coords = {
+  height: ddCoords.height,
+  width: ddCoords.width,
+  top: ddCoords.top - navCoords.top,
+  left: ddCoords.left - navCoords.left 
+}
+
+background.style.setProperty('width', `${coords.width}px`)
+background.style.setProperty('height', `${coords.height}px`)
+background.style.setProperty("transform", `translate(${coords.left}px, ${coords.top}px)`)
+}
+
+function handleExit() {
+  this.classList.remove('trigger-enter', '.trigger-enter-active')
+  background.classList.remove('open')
+}
+
+triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter))
+triggers.forEach(trigger => trigger.addEventListener('mouseleave', handleExit))
